1b71ef739725c32f54129cfded069b7b84582278
lynnl87 <lynnl87@gmail.com>
2021-05-01 19:31:31 -0500

    adjusted server with new api

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const config = require('./config');
 var con = mysql.createConnection(config.db);
 
 app.use(cors());
-app.get('/v1/users/:username', function(request, response) {
+app.get('/api/v1/users/:username', function(request, response) {
     var username = request.params.username;
       con.query("SELECT DISTINCT(player) FROM replay_data", function (err, result, fields) {
         if (err) throw err;
@@ -16,6 +16,16 @@ app.get('/v1/users/:username', function(request, response) {
     //response.send('Hello DELETE');
 });
 
+app.get('/api/v1/user_data/:username', function(request, response) {
+  var username = request.params.username;
+  console.log(username);
+    con.query("SELECT DISTINCT(hero), COUNT(hero) as totalPlayed , SUM(win = 2) as wins FROM `replay_data` WHERE player ='" + username + "' group by hero order by totalPlayed desc", function (err, result, fields) {
+      if (err) throw err;
+      return response.send(result);
+    });
+  console.log("Previous command is async?");
+  //response.send('Hello DELETE');
+});
 
 var server = app.listen(8081, function () {
    var host = server.address().address
