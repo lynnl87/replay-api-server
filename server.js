64f4dd9945e29aa3d8f20f2a0a2996722962b6c2
lynnl87 <lynnl87@gmail.com>
2021-05-16 14:48:54 -0500

    Organized the api's a little better.

--- server.js
+++ server.js
@@ -3,32 +3,36 @@ const cors = require('cors')
 var app = express();
 var mysql = require('mysql2');
 const config = require('./config');
+const util = require('util');
+
 var con = mysql.createConnection(config.db);
 
 app.use(cors());
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.get('/api/v1/users/:username', function (request, response) {
   var username = request.params.username;
   con.query("SELECT DISTINCT(player) FROM replay_data", function (err, result, fields) {
     if (err) throw err;
     return response.send(result);
   });
-  console.log("Previous command is async?");
-  //response.send('Hello DELETE');
 });
 
-app.get('/api/v1/users/:username/playerdata', function (request, response) {
+app.get('/api/v1/users/:username/data', function (request, response) {
   var username = request.params.username;
-  con.query("SELECT * FROM replay_data WHERE player ='" + username + "'", function (err, result, fields) {
+  con.query(util.format("SELECT * FROM replay_data WHERE player = '%s'", username), function (err, result, fields) {
     if (err) throw err;
     return response.send(result);
   });
-  console.log("Previous command is async?");
-  //response.send('Hello DELETE');
 });
 
 app.get('/api/v1/map_data/:username', function (request, response) {
   var username = request.params.username;
-  con.query("SELECT \
+  con.query(util.format("SELECT \
     map_data.*,\
     maps.map_name,\
     replay_data.win\
@@ -38,16 +42,17 @@ app.get('/api/v1/map_data/:username', function (request, response) {
     LEFT JOIN maps ON games.map_id = maps.id\
     LEFT JOIN replay_data on replay_data.games_id = games.id \
     WHERE\
-    map_data.player = '" + username + "' AND replay_data.player = '" + username + "'", function (err, result, fields) {
+    map_data.player = '%s' AND replay_data.player = '%s'", username, username), function (err, result, fields) {
     if (err) throw err;
     return response.send(result);
   });
 })
 
-app.get('/api/v1/user_data/:username', function (request, response) {
+app.get('/api/v1/users/:username/vs/:opponent', function (request, response) {
   var username = request.params.username;
+  var opponent = request.params.opponent
   console.log(username);
-  con.query("SELECT \
+  con.query(util.format("SELECT \
     mp1.games_id, \
     ANY_VALUE(mp1.player) as team_one_player, \
     ANY_VALUE(mp1.hero) as team_one_hero, \
@@ -62,19 +67,17 @@ FROM \
     replay_data mp2 \
 WHERE \
     mp1.games_id = mp2.games_id AND mp1.player != mp2.player AND(\
-        mp1.player IN('" + username + "') AND mp2.player IN('squacoon', '" + username + "')\
+        mp1.player IN('%s') AND mp2.player IN('%s', '%s')\
     )\
-GROUP BY mp1.games_id", function (err, result, fields) {
+GROUP BY mp1.games_id", username, opponent, username),  function (err, result, fields) {
     if (err) throw err;
     return response.send(result);
   });
-  console.log("Previous command is async?");
-  //response.send('Hello DELETE');
 });
 
 var server = app.listen(8081, function () {
   var host = server.address().address
   var port = server.address().port
 
-  console.log("Example app listening at http://%s:%s", host, port)
+  console.log("replay server listening at http://%s:%s", host, port)
 })
